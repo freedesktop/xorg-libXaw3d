de7d2c9eada6568689b5393bd2103ccc80cb3b2e
Rafał Mużyło <galtgendo@o2.pl>
2011-08-03 03:01:15 +0200

    Silence sentinel warnings
    Signed-off-by: Rafał Mużyło <galtgendo@o2.pl>
    Signed-off-by: Matt Turner <mattst88@gmail.com>

--- src/XawIm.c
+++ src/XawIm.c
@@ -674,7 +674,7 @@ static void SizeNegotiation(p, width, height)
     SetVaArg( &ic_a[ic_cnt], (XPointer) NULL);
 
     if (ic_cnt > 0) {
-	XGetICValues(p->xic, ic_a[0], ic_a[1], ic_a[2], ic_a[3], ic_a[4]);
+	XGetICValues(p->xic, ic_a[0], ic_a[1], ic_a[2], ic_a[3], ic_a[4], NULL);
 	if (pe_attr) XFree(pe_attr);
 	if (st_attr) XFree(st_attr);
 	if (p->xic == NULL) {
@@ -714,7 +714,7 @@ static void SizeNegotiation(p, width, height)
 	    SetVaArg( &ic_a[ic_cnt], (XPointer) pe_attr); ic_cnt++;
 	}
 	SetVaArg( &ic_a[ic_cnt], (XPointer) NULL);
-	XSetICValues(p->xic, ic_a[0], ic_a[1], ic_a[2], ic_a[3], ic_a[4]);
+	XSetICValues(p->xic, ic_a[0], ic_a[1], ic_a[2], ic_a[3], ic_a[4], NULL);
 	if (pe_attr) XFree(pe_attr);
 	if (st_attr) XFree(st_attr);
 	if (p->xic == NULL) {
@@ -831,7 +831,7 @@ static void CreateIC( w, ve )
 				   pe_a[4], pe_a[5], pe_a[6], pe_a[7], pe_a[8],
 				   pe_a[9], pe_a[10], pe_a[11], pe_a[12],
 				   pe_a[13], pe_a[14], pe_a[15], pe_a[16],
-				   pe_a[17], pe_a[18],  pe_a[19]);
+				   pe_a[17], pe_a[18],  pe_a[19], NULL);
 	SetVaArg( &ic_a[ic_cnt], (XPointer) XNPreeditAttributes); ic_cnt++;
 	SetVaArg( &ic_a[ic_cnt], (XPointer) pe_attr); ic_cnt++;
     }
@@ -842,7 +842,7 @@ static void CreateIC( w, ve )
 				   st_a[4], st_a[5], st_a[6], st_a[7], st_a[8],
 				   st_a[9], st_a[10], st_a[11], st_a[12],
 				   st_a[13], st_a[14], st_a[15], st_a[16],
-				   st_a[17], st_a[18],  st_a[19]);
+				   st_a[17], st_a[18],  st_a[19], NULL);
 	SetVaArg( &ic_a[ic_cnt], (XPointer) XNStatusAttributes); ic_cnt++;
 	SetVaArg( &ic_a[ic_cnt], (XPointer) st_attr); ic_cnt++;
     }
@@ -851,7 +851,7 @@ static void CreateIC( w, ve )
     p->xic = XCreateIC(ve->im.xim, ic_a[0], ic_a[1], ic_a[2], ic_a[3],
 		       ic_a[4], ic_a[5], ic_a[6], ic_a[7], ic_a[8], ic_a[9],
 		       ic_a[10], ic_a[11], ic_a[12], ic_a[13], ic_a[14],
-		       ic_a[15], ic_a[16], ic_a[17], ic_a[18], ic_a[19]);
+		       ic_a[15], ic_a[16], ic_a[17], ic_a[18], ic_a[19], NULL);
     if (pe_attr) XtFree(pe_attr);
     if (st_attr) XtFree(st_attr);
 
@@ -969,7 +969,7 @@ static void SetICValues( w, ve, focus )
 				      pe_a[4], pe_a[5], pe_a[6], pe_a[7],
 				      pe_a[8], pe_a[9], pe_a[10], pe_a[11],
 				      pe_a[12], pe_a[13], pe_a[14], pe_a[15],
-				      pe_a[16], pe_a[17], pe_a[18],  pe_a[19]);
+				      pe_a[16], pe_a[17], pe_a[18],  pe_a[19], NULL);
 	SetVaArg( &ic_a[ic_cnt], (XPointer) XNPreeditAttributes); ic_cnt++;
 	SetVaArg( &ic_a[ic_cnt], (XPointer) pe_attr); ic_cnt++;
     }
@@ -979,7 +979,7 @@ static void SetICValues( w, ve, focus )
 				      st_a[4], st_a[5], st_a[6], st_a[7],
 				      st_a[8], st_a[9], st_a[10], st_a[11],
 				      st_a[12], st_a[13], st_a[14], st_a[15],
-				      st_a[16], st_a[17], st_a[18],  st_a[19]);
+				      st_a[16], st_a[17], st_a[18],  st_a[19], NULL);
 	SetVaArg( &ic_a[ic_cnt], (XPointer) XNStatusAttributes); ic_cnt++;
 	SetVaArg( &ic_a[ic_cnt], (XPointer) st_attr); ic_cnt++;
     }
@@ -992,7 +992,7 @@ static void SetICValues( w, ve, focus )
 	XSetICValues(p->xic, ic_a[0], ic_a[1], ic_a[2], ic_a[3], ic_a[4],
 		     ic_a[5], ic_a[6], ic_a[7], ic_a[8], ic_a[9], ic_a[10],
 		     ic_a[11], ic_a[12], ic_a[13], ic_a[14], ic_a[15],
-		     ic_a[16], ic_a[17], ic_a[18], ic_a[19]);
+		     ic_a[16], ic_a[17], ic_a[18], ic_a[19], NULL);
 	if (pe_attr) XtFree(pe_attr);
 	if (st_attr) XtFree(st_attr);
     }
