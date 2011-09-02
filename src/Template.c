495853f8f93a990823b3ba48235ece1c23ca37bc
Gaetan Nadon <memsize@videotron.ca>
2011-09-02 11:01:29 -0400

    Template.c: use #include <X11/Xaw3d/TemplateP.h>
    rather than #include "TemplateP.h".
    
    Acked-by: Matt Turner <mattst88@gmail.com>
    Signed-off-by: Gaetan Nadon <memsize@videotron.ca>

--- src/Template.c
+++ src/Template.c
@@ -30,7 +30,7 @@ in this Software without prior written authorization from the X Consortium.
 #endif
 #include <X11/IntrinsicP.h>
 #include <X11/StringDefs.h>
-#include "TemplateP.h"
+#include <X11/Xaw3d/TemplateP.h>
 
 static XtResource resources[] = {
 #define offset(field) XtOffsetOf(TemplateRec, template.field)
